5798d69da2b0e1118f4715c995ba48ab74398934
Arsen <arsenohanyan@gmail.com>
2019-10-26 14:29:33 -0700

    adsada

---
+++ src/components/ScannedList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+import Layout from '../Layout';
+function Home() {
+    return (
+        <Layout>
+            <ul className ="ListContainer">
+                
+            </ul>
+        </Layout>
+    )
+}
+export default Home;
