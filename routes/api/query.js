0528bd049436b7739757f1e195225443c72947cb
StoneT2000 <stoneztao@gmail.com>
2019-10-26 23:41:36 -0700

    Update query.js

--- routes/api/query.js
+++ routes/api/query.js
@@ -3,10 +3,28 @@ const router = express.Router();
 const request = require('request');
 
 router.get('/test', (req, res) => res.send('Hey!'));
+router.post('/compute', (req, res) => {
+  console.log(req.body.Products);
+  var options = {
+    uri: "http://18.217.33.249:8444",
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    json: {
+    	"Products":req.body.Products
+        }
+    };
 
+  request(options, (err, resp, body) => {
+    console.log(resp);
+    console.log(err);
+    res.send(resp);
+  });
+
+  //res.send("Test");
+});
 router.post('/scan', (req, res) => {
-  let data =req.body.image;
-  //console.log(req.body);
 
   var options = {
     uri: "https://vaeip452tl.execute-api.us-east-2.amazonaws.com/OptiEatOCR",
@@ -15,11 +33,11 @@ router.post('/scan', (req, res) => {
       "image": req.body.image
     }
   };
-    request(options, (err, resp, body) => {
-console.log(resp);
-console.log(err);
-res.send(resp);
-    });
+  request(options, (err, resp, body) => {
+    console.log(resp);
+    console.log(err);
+    res.send(resp);
+  });
 
 });
 
