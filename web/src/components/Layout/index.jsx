d2cc776d9acb6c99d1a1f99e932f47a94a9181a8
Arsen <arsenohanyan@gmail.com>
2019-10-27 08:27:45 -0700

    few ui changes

--- web/src/components/Layout/index.jsx
+++ web/src/components/Layout/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './index.scss'
 import Header from '../Header';
 function Layout(props) {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+      
+        script.src = "https://developer.edamam.com/attribution/badge.js";
+        script.async = true;
+      
+        document.body.appendChild(script);
+      
+        return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
+
     return (
         <div className="Layout">
         <Header/>
             <div className="container">
             {props.children}
             </div>
+            <footer><div id="edamam-badge" data-color="white"></div></footer>
         </div>
     )
 }
